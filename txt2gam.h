6fc7d0199b2f688d986b97f7b53db152bacce12e
Sonnix <srsoft@nxt.ru>
2017-10-01 14:27:38 +0300

    add original txt2gam

---
+++ txt2gam.h
@@ -0,0 +1,28 @@
+/* Copyright (C) 2005-2010 Valeriy Argunov (nporep AT mail DOT ru) */
+/*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include "declarations.h"
+
+#ifndef QSP_MAINDEFINES
+	#define QSP_MAINDEFINES
+
+	#define QSP_GAMEID QSP_FMT("QSPGAME")
+	#define QSP_PASSWD QSP_FMT("No")
+	#define QSP_STARTLOC QSP_FMT("#")
+	#define QSP_ENDLOC QSP_FMT("-")
+
+#endif
