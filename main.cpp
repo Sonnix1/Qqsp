e248fe1fd5d6f71d795ab5d6f077fe628691ee3c
Sonnix <srsoft@nxt.ru>
2017-10-01 17:28:16 +0300

    bump version to v0.7

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName("Qqsp");
     a.setOrganizationName("Qqsp");
-    a.setApplicationVersion("0.5");
+    a.setApplicationVersion("0.7");
 
     QObject::tr("__LANGNAME__");
     QObject::tr("__LANGID__");
