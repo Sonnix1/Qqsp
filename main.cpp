55b6ba6cec64768fa426f39f94455069dca8d54c
Sonnix <srsoft@nxt.ru>
2018-02-16 19:12:48 +0300

    bump version to v1.4.2

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName("Qqsp");
     a.setOrganizationName("Qqsp");
-    a.setApplicationVersion("1.4.1");
+    a.setApplicationVersion("1.4.2");
     a.setDoubleClickInterval(1);
 
     QObject::tr("__LANGNAME__");
